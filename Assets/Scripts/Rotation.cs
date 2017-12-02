993d2714652563887aa98ccbc43c85388a0472a8
Dan Amador <danamadorpe@gmail.com>
2017-12-02 17:12:01 -0600

    Deleted extra variable

--- Assets/Scripts/Rotation.cs
+++ Assets/Scripts/Rotation.cs
@@ -5,11 +5,8 @@ public class Rotation : MonoBehaviour {
 
 
     void Update() {
-
-        Vector2 mouse = new Vector2(Input.mousePosition.x, Input.mousePosition.y);
         Vector2 pivotPoint = new Vector2(Camera.main.WorldToScreenPoint(transform.position).x, Camera.main.WorldToScreenPoint(transform.position).y);
-        Vector2 offset = new Vector2(mouse.x - pivotPoint.x, mouse.y - pivotPoint.y);
-        offset.Normalize();
+        Vector2 offset = new Vector2(Input.mousePosition.x - pivotPoint.x, Input.mousePosition.y - pivotPoint.y);
         float angle = Mathf.Atan2(offset.y, offset.x) * Mathf.Rad2Deg;
         transform.rotation = Quaternion.Euler(0, 0, angle - 90);
     }
